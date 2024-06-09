e594b2f222c34c40034d2af42e95e7e484fc03be
Kanishk <kanishk@23G-MACABL-0553.local>
2024-06-10 01:52:49 +0530

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,10 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	]
+kit: {
+    adapter: vercel()
+}
 };
 
 export default config;
